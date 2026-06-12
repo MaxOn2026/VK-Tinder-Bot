5364fb1e60a2dd66ed3ec85045af38151facabb9
maxon <vanvanov14@mail.ru>
2026-06-12 19:25:49 +0700

    edited db_config.py and vkinder_db.sql

--- database/models/db_config.py
+++ database/models/db_config.py
@@ -6,20 +6,20 @@ import os
 load_dotenv()
 
 DB_CONFIG = {
-    'host': 'localhost',
-    'port': 5432,
-    'database': 'vkinder_db',
-    'user': os.getenv('DB_USERNAME'),
-    'password': os.getenv('DB_PASSWORD')
+    'host': os.getenv('HOST'),
+    'port': os.getenv('PORT'),
+    'database': os.getenv('DATABASE'),
+    'user': os.getenv('USER'),
+    'password': os.getenv('PASSWORD')
     }
 
 def get_connection():
-    return psycopg2.connect(**DB_CONFIG, cursor_factory=RealDictCursor)
+    return psycopg2.connect(**DB_CONFIG)
 
 def execute_query(query, params=None):
     conn = get_connection()
     try:
-        with conn.cursor(cursor_factory=RealDictCursor) as cur:
+        with conn.cursor() as cur:
             cur.execute(query, params)
             if query.strip().upper().startswith('SELECT'):
                 return cur.fetchall()
